481952dea66f8e28911a6cc960e2d0463bc65114
Nicki Nixon <nickitza@gmail.com>
2019-05-13 16:58:49 -0600

    add update controller, some coosmetic edits

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,6 @@
 class NotesController < ApplicationController
   def index
-    @notes = Note.all
+    @notes = Note.order(:id)
   end
 
   def show
@@ -12,15 +12,33 @@ class NotesController < ApplicationController
   end
 
   def create
-    
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+
   end
 
   def edit
+    @note = Note.find(params[:id])
   end
 
   def update
+    @note = Note.find(params[:id])
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :author, :body)
   end
 end
